4a0abf0a90b1ddcf5bec438dfbf96b71069cc155
Rosen Toshev <toshev@usc.edu>
2018-09-17 18:04:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function appendKitten(name) {
   var kittens = ["Milo", "Otis", "Garfield"]
   
   function removeLastKitten() {
-      newKittens = kittens.slice(0, 1)
+      newKittens = kittens.slice(0, 2)
       
       return newKittens
   }
