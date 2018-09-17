e4b7ae4ed33867ecb00a0fa4d3375f3b049093ae
Rosen Toshev <toshev@usc.edu>
2018-09-17 17:29:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Add your functions and code here
-function kittens(array){
-  kittens = ["Milo", "Otis", "Garfield"]
+
+  var kittens = ["Milo", "Otis", "Garfield"]
 
 
 function destructivelyAppendKitten(name) {
-    kittens. 
+    kittens.push() 
 }
+destructivelyAppendKitten("Ralph")
+
+
 
